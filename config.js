b4a67dfec381adf068d8daa2dd6c15e298f9d7df
rodmarsh28 <39473318+rodmarsh28@users.noreply.github.com>
2023-01-09 09:50:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ config = {};
 
 config.SAVE_TO_DB = false;
 
-config.DB_HOST = "localhost";
-config.DB_USER = "root";
-config.DB_PASS = "";
+config.DB_HOST = $host;
+config.DB_USER = $user;
+config.DB_PASS = $pass;
 config.DB_NAME = "proxies";
 
 module.exports = config;
